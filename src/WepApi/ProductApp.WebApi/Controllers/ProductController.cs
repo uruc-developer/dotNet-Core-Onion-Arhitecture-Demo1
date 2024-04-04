2dda94af022c206da6b0e3d8a36f179d2fa048ea
uruc-developer@hpmini <urucs.dev@outlook.de>
2024-04-04 11:18:50 +0200

    WebApi controller without CQRS MeiatR implementet

---
+++ src/WepApi/ProductApp.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var alllist = await productRepository.GetAllAsync();
+            var result = alllist.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name
+            }).ToList();
+            return Ok(result);
+        }
+    }
+}
