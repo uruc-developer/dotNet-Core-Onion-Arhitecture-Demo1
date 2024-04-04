8a76f36fc348773767e55ca7e6083a661ff62054
uruc-developer@hpmini <urucs.dev@outlook.de>
2024-04-04 12:22:40 +0200

    AddPersistenceServices extension added

---
+++ src/Infrastructure/ProductApp.Persistence/ServiceRegitration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Persistence.Context;
+using ProductApp.Persistence.Repositories;
+
+namespace ProductApp.Persistence
+{
+    //Extention
+    public static class ServiceRegitration
+    {
+        public static void AddPersistenceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("memoryDb"));
+            serviceCollection.AddTransient<IProductRepository, ProductRepository>();
+        }
+    }
+}
