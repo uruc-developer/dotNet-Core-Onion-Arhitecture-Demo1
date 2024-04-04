cca3dc12920504e6c1d7331b6d7f98b7bb247f95
uruc-developer@hpmini <urucs.dev@outlook.de>
2024-04-04 10:30:36 +0200

    Persistence, Generic Repository added

---
+++ src/Infrastructure/ProductApp.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T : BaseEntity
+    {
+        public System.Threading.Tasks.Task<T> AddAsync(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Threading.Tasks.Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Threading.Tasks.Task<T> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/ProductApp.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProductApp.Persistence.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> AddAsync(Product Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
