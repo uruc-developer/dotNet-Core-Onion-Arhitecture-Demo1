bc6b47439347e85161d1ec85dfad8fa965f6674b
uruc-developer@hpmini <urucs.dev@outlook.de>
2024-04-04 09:04:59 +0200

    Application layer added, ProductDto added, Exception added

---
+++ src/Core/Product.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        //public ValidationException(): base("Validation error occured") alternative
+        public ValidationException(): this("Validation error occured")
+        {
+            
+        }
+        public ValidationException(string Message) : base(Message)
+        {
+            
+        }
+
+        public ValidationException(Exception ex) : this(ex.Message) 
+        {
+            
+        }
+    }
+}
