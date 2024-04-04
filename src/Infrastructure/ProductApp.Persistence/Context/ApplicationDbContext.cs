13dcc1d292ee92dc8eb23cb23425929992e7e876
uruc-developer@hpmini <urucs.dev@outlook.de>
2024-04-04 10:09:34 +0200

    Persistance, DbContext added

---
+++ src/Infrastructure/ProductApp.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Domain.Entities;
+using System;
+
+namespace ProductApp.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = Guid.NewGuid(), Name = "Book1", Value = 10, Quantity=100},
+                new Product() { Id = Guid.NewGuid(), Name = "Paper", Value = 1, Quantity = 300 },
+                new Product() { Id = Guid.NewGuid(), Name = "Pen", Value = 15, Quantity = 120 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
